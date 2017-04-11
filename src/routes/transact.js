150249378507d09c370ebce218cca2e32d84bd92
JWLD <jackwldavies@icloud.com>
2017-04-11 16:34:34 +0300

    moved joi validation from db_queries.js to the transact route #3

--- src/routes/transact.js
+++ src/routes/transact.js
@@ -1,10 +1,11 @@
+const Joi = require('joi');
 const connPool = require('../../database/db_connection.js');
 const dbQueries = require('../db_queries.js');
 
 const handler = (request, reply) => {
   dbQueries.postTransaction(connPool, request.payload, (err) => {
     if (err) {
-      return reply(`error when adding to database: ${err}`);
+      return reply(`Error when adding to database: ${err}`);
     }
     return reply.redirect('/');
   });
@@ -13,5 +14,13 @@ const handler = (request, reply) => {
 module.exports = {
   method: 'POST',
   path: '/transact',
-  handler
+  handler,
+  config: {
+    validate: {
+      payload: {
+        name: Joi.string().regex(/^[a-zA-Z]{3,40}$/),
+        amount: Joi.number().integer()
+      }
+    }
+  }
 };
