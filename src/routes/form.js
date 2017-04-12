3543f0ba432aca269c6ef4019e7f0c34dec7b1e4
JWLD <jackwldavies@icloud.com>
2017-04-12 21:33:25 +0300

    add logout route and add user pic to form view

--- src/routes/form.js
+++ src/routes/form.js
@@ -1,11 +1,26 @@
 const handler = (request, reply) => {
-  return reply.view('form', {
-    title: 'Update'
-  });
+  const data = {
+    title: 'Update',
+    loggedIn: false,
+    githubInfo: request.auth.credentials // JWT payload data
+  };
+
+  // if user is authenticated, set loggedIn bool to true
+  if (request.auth.isAuthenticated) {
+    data.loggedIn = true;
+  }
+
+  return reply.view('form', data);
 };
 
 module.exports = {
   method: 'GET',
   path: '/form',
-  handler
+  config: {
+    auth: {
+      mode: 'try', // handler will still run even if not authorised
+      strategy: 'jwtoken'
+    },
+    handler
+  }
 };
